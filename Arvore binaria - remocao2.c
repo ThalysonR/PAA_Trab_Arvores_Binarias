2b94bf16113a15e5770c9e057d94c3bdae96a56f
willfirstone <aoki.william@gmail.com>
2017-05-24 17:23:48 -0400

    Add files via upload

---
+++ Arvore binaria - remocao2.c
@@ -0,0 +1,178 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+typedef struct NoArvore
+{
+        int dado;
+        struct NoArvore *esquerda;
+        struct NoArvore *direita;
+
+}NoArvore;
+
+NoArvore* BuscarMenorValor(NoArvore *noh)
+{
+        if(noh==NULL)
+        {
+                /* Sem elementos na árvore */
+                return NULL;
+        }
+        if(noh->esquerda) /* Desce pela esquerda para achar o menor valor */
+                return BuscarMenorValor(noh->esquerda);
+        else 
+                return noh;
+}
+NoArvore* BuscarMaiorValor(NoArvore *noh)
+{
+        if(noh==NULL)
+        {
+                /* Sem valores na árvore */
+                return NULL;
+        }
+        if(noh->direita) /* Desce pela direita para achar o maior valor */
+                BuscarMaiorValor(noh->direita);
+        else 
+                return noh;
+}
+
+NoArvore * Inserir(NoArvore *noh,int dado)
+{
+        if(noh==NULL)
+        {
+                NoArvore *temp;
+                temp = (NoArvore *)malloc(sizeof(NoArvore));
+                temp -> dado = dado;
+                temp -> esquerda = temp -> direita = NULL;
+                return temp;
+        }
+
+        if(dado >(noh->dado))
+        {
+                noh->direita = Inserir(noh->direita,dado);
+        }
+        else if(dado < (noh->dado))
+        {
+                noh->esquerda = Inserir(noh->esquerda,dado);
+        }
+        /* Não faça nada, caso o dado já esteja na árvore */
+        return noh;
+
+}
+
+NoArvore *Remover(NoArvore *noh, int dado)
+{
+        NoArvore *temp;
+        if(noh==NULL)
+        {
+                printf("Valor nao encontrado\n\n");
+        }
+        else if(dado < noh->dado)
+        {
+                noh->esquerda = Remover(noh->esquerda, dado);
+        }
+        else if(dado > noh->dado)
+        {
+                noh->direita = Remover(noh->direita, dado);
+        }
+        else
+        {
+                /* Remover este nó e substituir tanto com o menor valor na direita ou então com o maior valor da esquerda. */
+                if(noh->direita && noh->esquerda)
+                {
+                        /* Substituir pelo menor valor na direita */
+                        temp = BuscarMenorValor(noh->direita);
+                        noh -> dado = temp->dado; 
+                        /* Remover o no utilizado, uma vez que substituimos com outro nó. */
+                        noh -> direita = Remover(noh->direita,temp->dado);
+                }
+                else
+                {
+                        /* Se tiver somente um ou zero filhos, remover diretamente da árvore e conectar o pai ao filho. */
+                        temp = noh;
+                        if(noh->esquerda == NULL)
+                                noh = noh->direita;
+                        else if(noh->direita == NULL)
+                                noh = noh->esquerda;
+                        free(temp); 
+                }
+        }
+        return noh;
+
+}
+
+NoArvore *Busca(NoArvore *noh, int dado)
+{
+        if(noh==NULL)
+        {
+                /* Se o valor não for encontrado */
+                return NULL;
+        }
+        if(dado > noh->dado)
+        {
+                /* Busca na direita */
+                return Busca(noh->direita,dado);
+        }
+        else if(dado < noh->dado)
+        {
+                /* Busca na esquerda */
+                return Busca(noh->esquerda,dado);
+        }
+        else
+        {
+                /* Valor encontrado */
+                return noh;
+        }
+}
+
+
+
+int main()
+{
+	NoArvore *raiz = NULL;
+	int N = NULL;
+	printf("Digite a quantidade de numeros que devem ser gerados aleatoriamente e sem repeticao: ");
+	scanf("%d", &N);
+
+
+	 printf("Gerando %d valores aleatorios para inserir na arvore:\n\n", N);
+ 	 int i = NULL;
+     int A[N];
+    
+	//Preenche um vetor com números em ordem crescente
+    for (i = 1; i < N+1; i++) {
+     A[i] = i;
+    }
+    
+    //Embaralha os números
+    for (i = 1; i < N+1; i++) {    
+    int temp = A[i];
+    int IndiceRandomico = rand() % N+1;
+
+    A[i] = A[IndiceRandomico];
+    A[IndiceRandomico] = temp;
+}
+
+    
+    printf("Valores gerados!\n\n");
+    
+    
+	//Inserção:
+	for(i = 1; i < N+1; i++){
+		raiz = Inserir(raiz, A[i]);
+	}
+    
+
+    //Remoção:
+    double t;
+	t = clock();
+	
+	int x;
+	printf("Digite o valor que deseje remover: ");
+	scanf("%d", &x);
+	
+	raiz = Remover(raiz,x);
+
+ t = (clock() - t)/CLOCKS_PER_SEC/1000;
+ printf("Tempo de execucao: %.10f milissegundos\n",t);
+ 
+}
---
+++ README - Arvore Binaria2.txt
@@ -0,0 +1,33 @@
+INSTRUÇÕES - ÁRVORE BINÁRIA
+
+Como estamos medindo a performance dos algoritmos, dividimos em 3 arquivos:
+a) Inserção
+b) Remoção
+c) Busca
+
+Para todos os programas, é gerado e preenchido um vetor de 1 a N números, e em seguida estes números são embaralhados e inseridos no vetor novamente.
+
+a) Inserção:
+1- Ao abrir a inserção, deve-se digitar a quantidade de números a serem gerados
+pelo programa.
+
+2- Logo em seguida é exibido o tempo que foi levado para realizar a operação
+de inserção
+
+-----------------------
+b) Remoção:
+1- Ao abrir a remoção, deve-se digitar a quantidade de números a serem gerados
+pelo programa;
+2- Digite o número a ser removido;
+3- Se a mensagem: "Valor não encontrado" for exposta, significa que o programa
+não encontrou o número desejado. Caso contrário, o valor foi removido
+4- É exibido o tempo que foi levado para realizar a operação de remoção.
+
+-----------------------
+c) Busca:
+1- Ao abrir a busca, deve-se digitar a quantidade de números a serem gerados
+pelo programa;
+2- Digite o número a ser buscado;
+3- Se a mensagem: "Valor xxx não encontrado" for exposta, significa que o programa
+não encontrou o número desejado. Caso contrário, o valor foi encontrado.
+4- É exibido o tempo que foi levado para realizar a operação de busca.
